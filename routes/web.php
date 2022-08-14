fbc8c5a4f27674fd1aef5e06de6f039699b8c4ff
root <root@ip-172-31-45-18.ap-south-1.compute.internal>
2022-08-14 11:39:18 +0000

    backup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Credentials: true');
+
 Route::get('/', function () {
     return redirect()->route('login');
 });
